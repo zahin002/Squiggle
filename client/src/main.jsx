c9b7dffa1d032c3f7f477bfdfd003e1798a92cb6
zahin002 <zahinuddinnirob001@gmail.com>
2026-05-30 21:59:20 +0600

    Fixing Main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import App from './App';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
